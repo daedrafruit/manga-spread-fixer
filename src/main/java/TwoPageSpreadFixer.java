c64fe1e494164a7252a1e7a50b88897d0c5f18a3
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2024-04-17 23:32:13 -0700

    good progress'

--- src/main/java/TwoPageSpreadFixer.java
+++ src/main/java/TwoPageSpreadFixer.java
@@ -6,83 +6,113 @@ import java.io.File;
 import java.io.IOException;
 
 public class TwoPageSpreadFixer {
+    static int pageCount = 0;
     public static void main(String[] args) {
         File folder = new File("test-folder");
         scanDirectory(folder);
     }
-    // function to recursively look through the files until it finds the directory with images
+
+    //function to recursively look through the files until it finds the directory with images
    public static void scanDirectory(File parent) {
+       //put contents of file in an array
        File[] children = parent.listFiles();
 
-      // return if there are no children
+       //return if there are no children
        if (children == null) {
            return;
        }
 
-       // for each child
+       //for each child
        for (File child : children) {
-           // if the child is a directory
+           //if the child is a directory
            if (child.isDirectory()) {
-               // recur scan inside
+               //reset page count
+               pageCount = 0;
+               //recur scan inside
                System.out.println("folder " + child.getName());
-              scanDirectory(child);
+               scanDirectory(child);
            }
+           //else the child is an image
            else {
-           // TODO: process images here
-           // once we reach the bottom directory we can scan the images
+               //once we reach the bottom directory we can scan the images
                System.out.println("image " + child.getName());
+
+               //load image from file
+               BufferedImage image = loadImage(child);
+
+               //make sure image is not null
+               if (image == null) {
+                   continue;
+               }
+
+
+               if (imageIsOffset(image)) {
+                   //insert a
+                   insertFillerPage(parent);
+               }
            }
       }
    }
+   public static boolean imageIsOffset(BufferedImage image) {
+       boolean landscape = false;
+       boolean onEvenPage = false;
 
-//   // TODO: rename variables "deepestFolder" and "folder" are confusing
-     // TODO: complete remake class, there doesnt need to be a loop
-//   public static void scanImages(File deepestFolder) {
-//       File[] folder = deepestFolder.listFiles();
-//       // keep count of current page number
-//       int pageCount = 0;
-//
-//       // TODO: make sure possible that folder can be null
-//       // return if no images
-//       if (folder == null) {
-//           return;
-//       }
-//
-//       // for each file in folder
-//       for (File file : folder) {
-//
-//           // load image from file
-//           BufferedImage image = loadImage(file);
-//
-//           // check if image is null
-//           if (image == null) {
-//               // done processing this file
-//               continue;
-//           }
-//
-//           // check if image is landscape
-//           if (image.getWidth() > image.getHeight()) {
-//               // increment page count by two (landscape means two pages)
-//               pageCount += 2;
-//
-//           }
-//           else {
-//               pageCount += 1;
-//           }
-//           System.out.println(pageCount);
-//
-//       }
-//   }
-//
-//   // method to load image from file
-//   public static BufferedImage loadImage(File file) {
-//       BufferedImage image = null;
-//       try {
-//           image = ImageIO.read(file);
-//       } catch (IOException e) {
-//           System.out.println("Error reading the image: " + file.getName());
-//       }
-//       return image;
-//   }
+       //check if image is landscape
+       if (image.getWidth() > image.getHeight()) {
+           //increment page count by two (landscape means two pages)
+           pageCount += 2;
+           landscape = true;
+       }
+       else {
+           pageCount += 1;
+       }
+       System.out.println(pageCount);
 
+       onEvenPage = (pageCount % 2 != 0);
+
+       //if the image is landscape and not on an even page, the image is offset
+       return landscape && !onEvenPage;
+   }
+
+   public  static void insertFillerPage(File folder) {
+       //check if a blank already exists in the file
+       System.out.println("inserting image: " + folder.getName());
+       File existingBlank = new File(folder.getName(), "!00.jpg");
+       if (existingBlank.exists()) {
+           //delete the image from the subfolder
+           existingBlank.delete();
+           System.out.println(folder + " Re-fixed (Image deleted)\n" +
+                   "The Spreads are inherently offset, fixing based on final spread in chapter.\n" +
+                   "Opening spreads such as cover-art may be off.");
+           //subtract from pageCount when image is deleted
+           pageCount -= 1;
+       }
+
+       System.out.println("Copied Blank Page");
+       //if not use the default "00.jpg"
+       File originalBlank = new File("images/!00.jpg");
+       BufferedImage blankImage = loadImage(originalBlank);
+       writeImage(blankImage, folder);
+
+   }
+
+   //method to load image from file
+   public static BufferedImage loadImage(File file) {
+       BufferedImage image = null;
+       try {
+           image = ImageIO.read(file);
+       } catch (IOException e) {
+           System.out.println("Error reading the image: " + file.getName());
+       }
+       return image;
+   }
+
+   //method to write image to file
+    public static void writeImage(BufferedImage image, File file) {
+        try {
+            ImageIO.write(image, "jpg", file);
+        } catch (IOException e) {
+            System.out.println("Error writing the image to: " + file);
+        }
+    }
 }
